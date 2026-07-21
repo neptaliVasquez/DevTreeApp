9a84123de610ce1fc9b3924cb7dc2cd125e61404
neptaliVasquez <64226980+neptaliVasquez@users.noreply.github.com>
2026-07-21 18:35:13 -0400

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,3 +7,6 @@ const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(colors.bgCyan.italic(`Server is running at http://localhost:${port}`));
 });
+
+
+https://teams.microsoft.com/meet/412509580895282?p=g6Ado3gdbmuUtBwgMu
