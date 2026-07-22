ed70ab23e1e3d0ef1cfd49059a5ba36a9f700583
neptaliVasquez <64226980+neptaliVasquez@users.noreply.github.com>
2026-07-22 11:00:34 -0400

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,3 +14,6 @@ https://teams.microsoft.com/meet/412509580895282?p=g6Ado3gdbmuUtBwgMu
 
 AHA CALL
 https://teams.microsoft.com/meet/238904215568136?p=T0Uuqz8rO66KPy0efq
+
+MULTI VENDOR
+https://teams.microsoft.com/meet/2492519311325?p=nFRcVp4X7swd4vzQ9T
